cb22a70ace8a1df67b6344a23eefc3544ea5a724
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:39:35 -0300

    Teste do uso de variável com o tipo de dados int em Java.

---
+++ src/TesteVariavelInt.java
@@ -0,0 +1,8 @@
+public class TesteVariavelInt {
+
+    public static void main(String[] args) {
+        int valorInteiroMaximo = 2147483647;
+
+        System.out.printf("O valor máximo do tipo int é: %d", valorInteiroMaximo);
+    }
+}
